31778236ff872c60c23f51287c4886aa3440fd54
BISCVITE30 <gonnki2004@gmail.com>
2026-06-28 16:55:14 +0200

    test commit

--- simpleCalculator.cs
+++ simpleCalculator.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 namespace Test;
 
@@ -34,8 +33,6 @@ class Program
         {
             if (answer == operation)
             {
-                Console.WriteLine(answer);
-                Console.WriteLine(operation);
                 isValid = true;
                 break;
             }
@@ -43,7 +40,6 @@ class Program
 
         if (!isValid)
         {
-            Console.WriteLine(!isValid);
             Console.WriteLine("Invalid operation");
             return;
         }
@@ -95,102 +91,4 @@ class Program
         
         Calculate();
     }
-=======
-using System;
-namespace Test;
-
-class Program
-{
-    static void Addition(double a, double b)
-    {
-        Console.WriteLine($"Your result is: {a + b}");
-    }
-    static void Subtraction(double a, double b)
-    {
-        Console.WriteLine($"Your result is: {a - b}");
-        
-    }
-    static void Multiplication(double a, double b)
-    {
-        Console.WriteLine($"Your result is: {a * b}");
-        
-    }
-    static void Division(double a, double b)
-    {
-        Console.WriteLine($"Your result is: {a / b}");
-
-    }
-    
-    static void Calculate()
-    {
-        Console.Write("Select an operation (+, -, *, /): ");
-        string answer = Console.ReadLine();
-        string[] operations = { "+", "-", "*", "/" };
-        bool isValid = false;
-        foreach (string operation in operations)
-        {
-            if (answer == operation)
-            {
-                Console.WriteLine(answer);
-                Console.WriteLine(operation);
-                isValid = true;
-                break;
-            }
-        }
-
-        if (!isValid)
-        {
-            Console.WriteLine(!isValid);
-            Console.WriteLine("Invalid operation");
-            return;
-        }
- 
-        Console.Write("Select number 1: ");
-        double num1 = Convert.ToDouble(Console.ReadLine());
-
-        Console.Write("Select number 2: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
-
-        switch (answer)
-        {
-            case "+":
-
-                Addition(num1, num2);
-                break;
-
-            case "-":
-
-                Subtraction(num1, num2);
-                break;
-
-            case "*":
-
-                Multiplication(num1, num2);
-                break;
-
-            case "/":
-
-                Division(num1, num2);
-                break;
-        }
-
-        Console.Write("do you want operation again? ");
-        string rep = Console.ReadLine();
-
-        if( rep == "Yes" || rep == "yes")
-        {
-            Calculate();
-        } else
-        {
-            return;
-        }
-    }
-
-    static void Main(string[] args)
-    {
-        
-        
-        Calculate();
-    }
->>>>>>> c15c47a3f794725553c5ed9cb33b02ecc9030261
 }
